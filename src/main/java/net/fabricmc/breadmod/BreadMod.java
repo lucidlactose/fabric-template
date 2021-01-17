ac1ec8ef4dfb64bbfdc4c1d36dec2f0c63c3fc7f
lucidlactose <jesusvillarruel98@gmail.com>
2021-01-16 23:21:39 -0800

    bread is now edible

--- src/main/java/net/fabricmc/breadmod/BreadMod.java
+++ src/main/java/net/fabricmc/breadmod/BreadMod.java
@@ -1,11 +1,18 @@
 package net.fabricmc.breadmod;
 
+import java.util.List;
+
+import com.mojang.datafixers.util.Pair;
+
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.breadmod.items.SlicedBread;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -13,7 +20,12 @@ import net.minecraft.util.registry.Registry;
 public class BreadMod implements ModInitializer {
 	public static final String MOD_ID = "breadmod";
 
-	public static final SlicedBread SLICED_BREAD = new SlicedBread(new FabricItemSettings().group(ItemGroup.FOOD).maxCount((16)));
+	public static final FoodComponent foodSettings = new FoodComponent.Builder().hunger(1).saturationModifier(1.0f).snack().meat().alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.POISON, 20*3), .5f).build());
+	public static final SlicedBread SLICED_BREAD = new SlicedBread(
+		new FabricItemSettings()
+		.group(ItemGroup.FOOD)
+		.maxCount((16))
+		.food(foodSettings));
 	public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).hardness(4.0f));
 
 	@Override
--- src/main/java/net/fabricmc/breadmod/items/SlicedBread.java
+++ src/main/java/net/fabricmc/breadmod/items/SlicedBread.java
@@ -1,23 +1,10 @@
 package net.fabricmc.breadmod.items;
 
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.world.World;
 
 public class SlicedBread extends Item {
 
     public SlicedBread(Settings settings) {
         super(settings);
     }
-    
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
-        playerEntity.playSound(SoundEvents.ENTITY_GENERIC_EAT, 1.0f, 1.0f);
-        return TypedActionResult.success(playerEntity.getStackInHand(hand));
-    }
-
 }
